8c9488ba2500adab5799c5e7ed5930edbf01d6d1
Neeraj <neerajsaraswat111@gmail.com>
2024-04-16 15:39:43 +0530

    edit deleteVideo controller so that after deleting video all related likes, comments will delete and also video will delete from all playlists

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,94 +1,100 @@
 import mongoose, { isValidObjectId } from "mongoose";
 import { Video } from "../models/video.model.js";
 import { User } from "../models/user.model.js";
+import { Comment } from "../models/comment.model.js";
+import { Like } from "../models/like.model.js";
+import { Playlist } from "../models/playlist.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { uploadOnCloudinary,deleteImageFromCloudinary } from "../utils/cloudinary.js";
+import {
+  uploadOnCloudinary,
+  deleteImageFromCloudinary,
+} from "../utils/cloudinary.js";
 
 const getAllVideos = asyncHandler(async (req, res) => {
   const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
   //TODO: get all videos based on query, sort, pagination
 
-try {
-  const pipeline = [];
+  try {
+    const pipeline = [];
 
-  if (!userId) {
-    throw new ApiError(400, "User Id not provided");
-  }
+    if (!userId) {
+      throw new ApiError(400, "User Id not provided");
+    }
 
-  if (!isValidObjectId(userId)) {
-    throw new ApiError(400, "Invalid User Id");
-  }
+    if (!isValidObjectId(userId)) {
+      throw new ApiError(400, "Invalid User Id");
+    }
 
-  if (userId) {
-    pipeline.push({
-      $match: {
-        owner: new mongoose.Types.ObjectId(userId),
-      },
-    });
-  }
+    if (userId) {
+      pipeline.push({
+        $match: {
+          owner: new mongoose.Types.ObjectId(userId),
+        },
+      });
+    }
 
-  if (query) {
-    pipeline.push({
-      $match: {
-        title: {
-          $regex: query,
+    if (query) {
+      pipeline.push({
+        $match: {
+          title: {
+            $regex: query,
+          },
+        },
+      });
+    }
+
+    pipeline.push({ $match: { isPublished: true } });
+
+    pipeline.push(
+      {
+        $lookup: {
+          from: "users",
+          localField: "owner",
+          foreignField: "_id",
+          as: "owner",
+          pipeline: [
+            {
+              $project: {
+                username: 1,
+                fullname: 1,
+                avatar: 1,
+              },
+            },
+          ],
         },
       },
-    });
-  }
+      {
+        $unwind: "$owner",
+      }
+    );
 
-  pipeline.push({ $match: { isPublished: true } });
+    const aggregate = Video.aggregate(pipeline);
 
-  pipeline.push(
-    {
-      $lookup: {
-        from: "users",
-        localField: "owner",
-        foreignField: "_id",
-        as: "owner",
-        pipeline: [
-          {
-            $project: {
-              username: 1,
-              fullname: 1,
-              avatar: 1,
-            },
-          },
-        ],
+    const options = {
+      page,
+      limit,
+      sort: {
+        [sortBy]: sortType,
       },
-    },
-    {
-      $unwind: "$owner",
-    }
-  );
+    };
 
-  const aggregate = Video.aggregate(pipeline);
+    const videos = await Video.aggregatePaginate(aggregate, options);
 
-  const options = {
-    page,
-    limit,
-    sort: {
-      [sortBy]: sortType,
-    },
-  };
-
-  const videos = await Video.aggregatePaginate(aggregate, options);
-
-  return res
-    .status(200)
-    .json(new ApiResponse(200, videos.docs, "Videos fetched successfully"));
-} catch (error) {
-     return res
-       .status(500)
-       .json(
-         new ApiError(
-           500,
-           error?.message || "Something went wrong while fetching videos"
-         )
-       );
-}
+    return res
+      .status(200)
+      .json(new ApiResponse(200, videos.docs, "Videos fetched successfully"));
+  } catch (error) {
+    return res
+      .status(500)
+      .json(
+        new ApiError(
+          500,
+          error?.message || "Something went wrong while fetching videos"
+        )
+      );
+  }
 });
 
 const publishAVideo = asyncHandler(async (req, res) => {
@@ -145,8 +151,8 @@ const getVideoById = asyncHandler(async (req, res) => {
 
   const video = await Video.findById(videoId);
 
-  if(!video){
-    throw new ApiError(404,"video not found")
+  if (!video) {
+    throw new ApiError(404, "video not found");
   }
 
   return res
@@ -225,9 +231,38 @@ const deleteVideo = asyncHandler(async (req, res) => {
   await deleteImageFromCloudinary(video.videoFile);
   await deleteImageFromCloudinary(video.thumbnail);
 
+  await Comment.deleteMany({ video: videoId });
+
+  await Like.deleteMany({ video: videoId });
+
+  try {
+    const playlists = await Playlist.find({ videos: videoId });
+
+    const promises = playlists.map(async (playlist) => {
+      playlist.videos = playlist.videos.filter(
+        (id) => id.toString() !== videoId.toString()
+      );
+
+      await playlist.save({ validateBeforeSave: false });
+    });
+
+    await Promise.all(promises);
+  } catch (error) {
+    throw new ApiError(
+      500,
+      `Error while removing video from playlists: ${error.message}`
+    );
+  }
+
   return res
     .status(200)
-    .json(new ApiResponse(200, {}, "video deleted succesfully"));
+    .json(
+      new ApiResponse(
+        200,
+        {},
+        "video deleted and removed from all playlists, Also likes and comments associated with the video removed successfully"
+      )
+    );
 });
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
