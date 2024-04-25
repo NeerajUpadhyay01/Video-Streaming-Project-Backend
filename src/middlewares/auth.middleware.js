3f9b1708fc9b23b25a247f40c6c8c5a36d8ea341
Neeraj <neerajsaraswat111@gmail.com>
2024-04-25 16:39:38 +0530

    bug fixed

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -10,7 +10,7 @@ export const verifyJWT = asyncHandler(async (req, _ , next) => {     //res did n
       req.header("Authorization")?.replace("Bearer ", "");
 
     if (!token) {
-      new ApiError(401, "Unauthorized request");
+      throw new ApiError(401, "Unauthorized request");
     }
 
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
