8a05ba871bfde50839e7122ca1be6c74f4841e48
Neeraj <neerajsaraswat111@gmail.com>
2024-07-20 16:45:05 +0530

    cors config options changed

--- src/app.js
+++ src/app.js
@@ -8,8 +8,8 @@ app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    secure:true,
+    sameSite:"none"
   })
 );
 
